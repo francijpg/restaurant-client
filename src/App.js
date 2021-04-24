d34d7dfaa564a68f38a397ee6a13e0b24b2ad5bf
francijpg <hellofrancijpg@gmail.com>
2021-04-24 10:29:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router";
+import Orders from "./components/pages/Orders";
+import Menu from "./components/pages/Menu";
+import NewDish from "./components/pages/NewDish";
+import Sidebar from "./components/ui/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="md:flex min-h-screen">
+      <Sidebar />
+      <div className="md:w-2/5 xl:w-4/5 p-6">
+        <Routes>
+          <Route path="/" element={<Orders />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/new-dish" element={<NewDish />} />
+        </Routes>
+      </div>
     </div>
   );
 }
