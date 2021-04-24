f6b933ea34a3afb813b677cd51c1970f0e24b90c
francijpg <hellofrancijpg@gmail.com>
2021-04-24 10:30:31 -0400

    Create Orders.js

---
+++ src/components/pages/Orders.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Orders = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-light mb-4">Orders</h1>
+      {/* <div className="sm:flex sm:flex-wrap -mx-3">list</div> */}
+    </>
+  );
+};
+
+export default Orders;
