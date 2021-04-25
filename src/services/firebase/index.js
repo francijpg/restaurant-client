fb31d29928ed5f30071bc7863eadc682c7844c47
francijpg <hellofrancijpg@gmail.com>
2021-04-25 08:48:41 -0400

    Create index.js

---
+++ src/services/firebase/index.js
@@ -0,0 +1,19 @@
+import app from "firebase/app";
+import "firebase/firestore";
+import "firebase/storage";
+
+import firebaseConfig from "./config";
+
+if (!app.apps.length) {
+  app.initializeApp(firebaseConfig);
+}
+
+export const firebaseApp = app;
+export const firestore = app.firestore();
+export const storage = app.storage();
+export const database = {
+  orders: firestore.collection("orders"),
+  formatDoc: (doc) => {
+    return { id: doc.id, ...doc.data() };
+  },
+};
