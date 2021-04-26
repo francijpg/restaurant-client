e720089a5c7a3d7ff817badcf057f8e0773aa308
francijpg <hellofrancijpg@gmail.com>
2021-04-26 11:13:55 -0400

    Create Sidebar.js

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <div className="md:w-2/6 xl:w-1/5 bg-gray-800">
+      <div className="p-6">
+        <p className="uppercase text-white text-2xl tracking-wide text-center font-bold">
+          Restaurant App
+        </p>
+
+        <p className="mt-3 text-gray-600">
+          Manage your restaurant with the following options:
+        </p>
+
+        <nav className="mt-10">
+          <NavLink
+            className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900"
+            activeClassName="text-yellow-500"
+            end
+            to="/"
+          >
+            Orders
+          </NavLink>
+          <NavLink
+            className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900"
+            activeClassName="text-yellow-500"
+            end
+            to="/menu"
+          >
+            Menu
+          </NavLink>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
