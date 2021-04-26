824dc3c369d71242fe7c2ed1e07a323d3b740843
francijpg <hellofrancijpg@gmail.com>
2021-04-26 11:14:26 -0400

    Update Orders.js

--- src/components/pages/Orders.js
+++ src/components/pages/Orders.js
@@ -1,9 +1,14 @@
 import React from "react";
+// import { useStorage } from "../../contexts/StorageContext";
 
 const Orders = () => {
+  // const { services, printService, listServiceTwo } = useStorage();
+  // const { listServiceTwo } = useContext(FirebaseContext);
+
   return (
     <>
       <h1 className="text-3xl font-light mb-4">Orders</h1>
+      {/* <button onClick={() => console.log(services)}>FB</button> */}
       {/* <div className="sm:flex sm:flex-wrap -mx-3">list</div> */}
     </>
   );
