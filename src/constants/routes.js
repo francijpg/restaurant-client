b838a9abdeb9deb14d79871082a44ffcac233691
francijpg <hellofrancijpg@gmail.com>
2021-05-07 09:18:05 -0400

    Create routes.js

---
+++ src/constants/routes.js
@@ -0,0 +1,8 @@
+export const DASHBOARD = "/";
+export const DISHES_MENU = "/menu";
+export const NEW_DISH = "/new-dish";
+export const NOT_FOUND = "*";
+// USER
+export const LOGIN = "/login";
+export const SIGN_UP = "/sign-up";
+export const PROFILE = "/p/:username";
