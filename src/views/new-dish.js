910921b5a20e4f340cc8dcc0154a99bb36478f3a
francijpg <hellofrancijpg@gmail.com>
2021-05-07 09:20:31 -0400

    finish new-dish view

---
+++ src/views/new-dish.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FormRegisterDish from "../components/dishes/molecules/FormRegisterDish";
+
+const NewDish = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-light mb-4">New Dishes</h1>
+      <FormRegisterDish />
+    </>
+  );
+};
+
+export default NewDish;
