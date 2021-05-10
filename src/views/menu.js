094f563e36e06c7bd7f0bc62b03b1c311b72df08
francijpg <hellofrancijpg@gmail.com>
2021-05-10 11:59:40 -0400

    Update menu.js

--- src/views/menu.js
+++ src/views/menu.js
@@ -1,25 +1,32 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import useDishes from "../hooks/useDishes";
+import React, { useEffect } from "react";
+
+import Title from "../components/common/Title";
+import Link from "../components/common/Link";
 import DishDetail from "../components/dishes/molecules/DishDetail";
+
 import * as ROUTES from "../constants/routes";
+
+import useDishes from "../hooks/useDishes";
+import { useStorage } from "../contexts/StorageContext";
 
 const Menu = () => {
   const { dishes } = useDishes("name");
+  const { storageError, setStorageError } = useStorage();
+
+  useEffect(() => {
+    document.title = "Menu - RestaurantApp";
+    setStorageError("");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
-      <h1 className="text-3xl font-light mb-4">Menu</h1>
-      <Link
-        to={ROUTES.NEW_DISH}
-        className="  bg-blue-800 hover:bg-blue-700, inline-block mb-5 p-2 text-white uppercase font-bold"
-      >
-        Add Dish
-      </Link>
-
-      {dishes.map((dish) => (
-        <DishDetail key={dish.id} dish={dish} />
-      ))}
+      <Title>menu</Title>
+      {storageError && (
+        <p className="mb-4 text-xs text-red-primary">{storageError}</p>
+      )}
+      <Link route={ROUTES.NEW_DISH}>add dish</Link>
+      {dishes && dishes.map((dish) => <DishDetail key={dish.id} dish={dish} />)}
     </>
   );
 };
