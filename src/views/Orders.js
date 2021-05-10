6014c7f8e1f64b9b9175a75c378a68fab888892a
francijpg <hellofrancijpg@gmail.com>
2021-05-10 11:59:26 -0400

    Update Orders.js

--- src/views/Orders.js
+++ src/views/Orders.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Title from "../components/common/Title";
 import Order from "../components/orders/molecules/Order";
 import useOrders from "../hooks/useOrders";
 
 const Orders = () => {
   const { orders } = useOrders;
+
+  useEffect(() => {
+    document.title = "Orders - RestaurantApp";
+  }, []);
+
   return (
     <>
-      <h1 className="text-3xl font-light mb-4">Orders</h1>
+      <Title>orders</Title>
       <div className="sm:flex sm:flex-wrap -mx-3">
         {!!orders &&
           orders.map((order) => <Order key={order.id} order={order} />)}
