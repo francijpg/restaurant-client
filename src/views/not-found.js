5b0962975430c2ebbeb6ca042a187c7084f84b75
francijpg <hellofrancijpg@gmail.com>
2021-05-10 11:59:31 -0400

    Update not-found.js

--- src/views/not-found.js
+++ src/views/not-found.js
@@ -1,13 +1,17 @@
 import { useEffect } from "react";
+import Link from "../components/common/Link";
+import Title from "../components/common/Title";
+import * as ROUTES from "../constants/routes";
 
 export default function NotFound() {
   useEffect(() => {
-    document.title = "Not Found - Restaurant App";
+    document.title = "Not Found - RestaurantApp";
   }, []);
 
   return (
     <div className="mx-auto max-w-screen-lg">
-      <p className="text-center text-2xl">Not Found!</p>
+      <Title>not found!</Title>
+      <Link route={ROUTES.DASHBOARD}>back to homepage</Link>
     </div>
   );
 }
