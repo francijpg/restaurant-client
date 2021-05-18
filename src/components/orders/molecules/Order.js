9f4b005f6586fb3d77a1d647871bd50e858eea69
francijpg <hellofrancijpg@gmail.com>
2021-05-18 08:38:57 -0400

    Update Order.js

--- src/components/orders/molecules/Order.js
+++ src/components/orders/molecules/Order.js
@@ -10,8 +10,8 @@ const Order = ({ order }) => {
     <div className="sm:w-1/2 lg:w-1/3 px-2 mb-4">
       <div className="p-3 shadow-md bg-white">
         <h1 className="text-yellow-600 text-lg font-bold"> {order.id} </h1>
-        {order.order.map((dishes) => (
-          <p className="text-gray-600">
+        {order.order.map((dishes, i) => (
+          <p key={i} className="text-gray-600">
             {dishes.amount} {dishes.name}
           </p>
         ))}
