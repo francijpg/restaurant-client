0e5d4239fa23adf9fe1cc5f3b49a3e3a2b2832f5
francijpg <hellofrancijpg@gmail.com>
2021-05-19 09:20:28 -0400

    Update Title.js

--- src/components/common/Title.js
+++ src/components/common/Title.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Title = ({ children }) => {
-  return <h1 className="text-3xl font-light mb-4 capitalize">{children}</h1>;
+const Title = ({ align = "left", children }) => {
+  return (
+    <h1 className="text-3xl font-light mb-4 capitalize" align={align}>
+      {children}
+    </h1>
+  );
 };
 
 export default Title;
