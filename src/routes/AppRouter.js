89f6182f0332c3550dfd4529f44d1217de37233d
francijpg <hellofrancijpg@gmail.com>
2021-05-19 09:21:18 -0400

    Update AppRouter.js

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import * as ROUTES from "../constants/routes";
 
@@ -8,25 +8,35 @@ import Orders from "../views/orders";
 import Menu from "../views/menu";
 import NewDish from "../views/new-dish";
 import NotFound from "../views/not-found";
+import LogIn from "../views/log-in";
+import SignUp from "../views/sign-up";
 
-import StorageContext from "../contexts/StorageContext";
+import { StorageProvider } from "../contexts/StorageContext";
+import { AuthProvider } from "../contexts/AuthContext";
+import PrivateRoute from "./PrivateRoute";
+import Header from "../components/layout/Header";
 
 function AppRouter() {
   return (
     <Router>
-      <StorageContext>
-        <div className="md:flex min-h-screen">
-          <Sidebar />
-          <div className="md:w-2/5 xl:w-4/5 p-6">
-            <Routes>
-              <Route path={ROUTES.DASHBOARD} element={<Orders />} />
-              <Route path={ROUTES.DISHES_MENU} element={<Menu />} />
-              <Route path={ROUTES.NEW_DISH} element={<NewDish />} />
-              <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
-            </Routes>
+      <AuthProvider>
+        <StorageProvider>
+        <Header />
+          <div className="md:flex min-h-screen">
+            <Sidebar />
+            <div className="md:w-2/5 xl:w-4/5 p-6">
+              <Switch>
+                <Route path={ROUTES.SIGN_UP} component={SignUp} />
+                <Route path={ROUTES.LOGIN} component={LogIn} />
+                <PrivateRoute exact path={ROUTES.DASHBOARD} component={Orders} />
+                <Route path={ROUTES.DISHES_MENU} component={Menu} />
+                <Route path={ROUTES.NEW_DISH} component={NewDish} />
+                <Route path={ROUTES.NOT_FOUND} component={NotFound} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </StorageContext>
+        </StorageProvider>
+      </AuthProvider>
     </Router>
   );
 }
