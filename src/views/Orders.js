7c8f9375e3405f06e13cf2b401f943457c47757c
francijpg <hellofrancijpg@gmail.com>
2021-05-20 10:27:34 -0400

    Update Orders.js

--- src/views/Orders.js
+++ src/views/Orders.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import Title from "../components/common/Title";
 import Order from "../components/orders/molecules/Order";
+import { useAuth } from "../contexts/AuthContext";
 import { useStorage } from "../contexts/StorageContext";
 import useOrders from "../hooks/useOrders";
 
 const Orders = () => {
   const { orders } = useOrders();
   const { storageError, setStorageError } = useStorage();
+  const { needVerification } = useAuth();
 
   useEffect(() => {
     document.title = "Orders - RestaurantApp";
@@ -17,6 +19,7 @@ const Orders = () => {
   return (
     <>
       <Title>orders</Title>
+      {needVerification && <p>Please verify your email address.</p>}
       {storageError && (
         <p className="mb-4 text-xs text-red-primary">{storageError}</p>
       )}
