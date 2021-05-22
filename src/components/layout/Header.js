951d62d37a1a8d48d385069f4541d8a2c5a35a76
francijpg <hellofrancijpg@gmail.com>
2021-05-22 12:46:11 -0400

    Update Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,20 +1,16 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link, useHistory } from "react-router-dom";
 
 import * as ROUTES from "../../constants/routes";
+import * as ICONS from "../../constants/icons";
 import { useAuth } from "../../contexts/AuthContext";
-// import { useAuth } from "../../contexts/AuthContext";
-// import useAuthListener from "../../hooks/useAuthListener";
-import useUser from "../../hooks/useUser";
+// import useUser from "../../hooks/useUser";
 
 const Header = () => {
   const { currentUser, setLogOut } = useAuth();
-  // const { setLogOut } = useAuth();
-  // const {currentUser: loggedInUser} = useAuthListener()
-  const { user } = useUser(currentUser?.uid);
+  // const { user } = useUser(currentUser?.uid);
   const history = useHistory();
-
-  // useEffect(() => {}, [currentUser]);
+  const profileImg = currentUser?.displayName.toLowerCase();
 
   return (
     <header className="h-16 bg-white border-b border-gray-primary">
@@ -23,41 +19,15 @@ const Header = () => {
           <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
             <h1 className="flex justify-center w-full">
               <Link to={ROUTES.DASHBOARD} aria-label="RestaurantApp logo">
-                <svg
-                  className="w-8 mr-6 text-black-light cursor-pointer"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                  />
-                </svg>
+                {ICONS.HOME}
               </Link>
             </h1>
           </div>
           <div className="text-gray-700 text-center flex items-center align-items">
-            {user ? (
+            {currentUser ? (
               <>
                 <Link to={ROUTES.DASHBOARD} aria-label="Dashboard">
-                  <svg
-                    className="w-8 mr-6 text-black-light cursor-pointer"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                    />
-                  </svg>
+                  {ICONS.HOME}
                 </Link>
 
                 <button
@@ -68,27 +38,14 @@ const Header = () => {
                     history.push(ROUTES.LOGIN);
                   }}
                 >
-                  <svg
-                    className="w-8 mr-6 text-black-light cursor-pointer"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                    />
-                  </svg>
+                  {ICONS.LOG_OUT}
                 </button>
                 <div className="flex items-center cursor-pointer">
-                  <Link to={`/p/${user?.username}`}>
+                  <Link to={`/p/${profileImg}`}>
                     <img
                       className="rounded-full h-8 w-8 flex"
-                      src={`/images/avatars/${user.username}.jpg`}
-                      alt={`${user?.username} profile`}
+                      src={`/images/avatars/${profileImg}.jpg`}
+                      alt={`${profileImg} profile`}
                     />
                   </Link>
                 </div>
