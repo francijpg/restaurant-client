7195dc5dc0471a6d59c71d114ad5449aabf1921a
francijpg <hellofrancijpg@gmail.com>
2021-05-24 12:39:50 -0400

    Update Orders.js

--- src/views/Orders.js
+++ src/views/Orders.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import Title from "../components/common/Title";
+import Sidebar from "../components/layout/Sidebar";
 import Order from "../components/orders/molecules/Order";
 import { useAuth } from "../contexts/AuthContext";
 import { useStorage } from "../contexts/StorageContext";
@@ -18,14 +19,19 @@ const Orders = () => {
 
   return (
     <>
-      <Title>orders</Title>
-      {needVerification && <p>Please verify your email address.</p>}
-      {storageError && (
-        <p className="mb-4 text-xs text-red-primary">{storageError}</p>
-      )}
-      <div className="sm:flex sm:flex-wrap -mx-3">
-        {!!orders &&
-          orders.map((order) => <Order key={order.id} order={order} />)}
+      <div className="md:flex min-h-screen">
+        <Sidebar />
+        <div className="md:w-2/5 xl:w-4/5 p-6">
+          <Title>orders</Title>
+          {needVerification && <p>Please verify your email address.</p>}
+          {storageError && (
+            <p className="mb-4 text-xs text-red-primary">{storageError}</p>
+          )}
+          <div className="sm:flex sm:flex-wrap -mx-3">
+            {!!orders &&
+              orders.map((order) => <Order key={order.id} order={order} />)}
+          </div>
+        </div>
       </div>
     </>
   );
