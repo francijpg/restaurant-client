0a40447c8f7cc3cb8ee709ce43528ea831a5f147
francijpg <hellofrancijpg@gmail.com>
2021-05-24 12:43:20 -0400

    Update new-dish.js

--- src/views/new-dish.js
+++ src/views/new-dish.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Title from "../components/common/Title";
 import FormRegisterDish from "../components/dishes/molecules/FormRegisterDish";
+import Sidebar from "../components/layout/Sidebar";
 
 const NewDish = () => {
   useEffect(() => {
@@ -9,8 +10,13 @@ const NewDish = () => {
 
   return (
     <>
-      <Title>new dish</Title>
-      <FormRegisterDish />
+      <div className="md:flex min-h-screen">
+        <Sidebar />
+        <div className="md:w-2/5 xl:w-4/5 p-6">
+          <Title>new dish</Title>
+          <FormRegisterDish />
+        </div>
+      </div>
     </>
   );
 };
